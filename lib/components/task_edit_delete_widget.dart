be4fdc6d906bc298cb8d75c547d19eea8aca711f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-12 10:34:41 +0000

    Updating to latest FlutterFlow output.

--- lib/components/task_edit_delete_widget.dart
+++ lib/components/task_edit_delete_widget.dart
@@ -43,10 +43,10 @@ class _TaskEditDeleteWidgetState extends State<TaskEditDeleteWidget> {
           borderRadius: BorderRadius.circular(12.0),
           child: Container(
             constraints: const BoxConstraints(
-              minWidth: 100.0,
-              minHeight: 60.0,
-              maxWidth: 100.0,
-              maxHeight: 63.0,
+              minWidth: 140.0,
+              minHeight: 80.0,
+              maxWidth: 140.0,
+              maxHeight: 80.0,
             ),
             decoration: BoxDecoration(
               color: FlutterFlowTheme.of(context).secondaryBackground,
@@ -64,77 +64,82 @@ class _TaskEditDeleteWidgetState extends State<TaskEditDeleteWidget> {
             ),
             child: Align(
               alignment: const AlignmentDirectional(0.0, 0.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: FlutterFlowTheme.of(context).secondaryBackground,
-                    ),
-                    child: Padding(
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(
-                            Icons.edit_sharp,
-                            color: FlutterFlowTheme.of(context).primaryText,
-                            size: 20.0,
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Edit',
-                              style: FlutterFlowTheme.of(context)
-                                  .bodyMedium
-                                  .override(
-                                    fontFamily: 'Inter',
-                                    letterSpacing: 0.0,
-                                  ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: FlutterFlowTheme.of(context).secondaryBackground,
+                      ),
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(
+                              Icons.edit_sharp,
+                              color: FlutterFlowTheme.of(context).primaryText,
+                              size: 24.0,
                             ),
-                          ),
-                        ].divide(const SizedBox(width: 8.0)),
+                            Expanded(
+                              child: Text(
+                                'Edit',
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .override(
+                                      fontFamily: 'Inter',
+                                      fontSize: 16.0,
+                                      letterSpacing: 0.0,
+                                    ),
+                              ),
+                            ),
+                          ].divide(const SizedBox(width: 8.0)),
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: FlutterFlowTheme.of(context).secondaryBackground,
-                    ),
-                    child: Padding(
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(
-                            Icons.delete_outline_rounded,
-                            color: FlutterFlowTheme.of(context).error,
-                            size: 20.0,
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Delete',
-                              style: FlutterFlowTheme.of(context)
-                                  .bodyMedium
-                                  .override(
-                                    fontFamily: 'Inter',
-                                    color: FlutterFlowTheme.of(context).error,
-                                    letterSpacing: 0.0,
-                                  ),
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: FlutterFlowTheme.of(context).secondaryBackground,
+                      ),
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(
+                              Icons.delete_outline_rounded,
+                              color: FlutterFlowTheme.of(context).error,
+                              size: 24.0,
                             ),
-                          ),
-                        ].divide(const SizedBox(width: 8.0)),
+                            Expanded(
+                              child: Text(
+                                'Delete',
+                                style: FlutterFlowTheme.of(context)
+                                    .bodyMedium
+                                    .override(
+                                      fontFamily: 'Inter',
+                                      color: FlutterFlowTheme.of(context).error,
+                                      fontSize: 16.0,
+                                      letterSpacing: 0.0,
+                                    ),
+                              ),
+                            ),
+                          ].divide(const SizedBox(width: 8.0)),
+                        ),
                       ),
                     ),
-                  ),
-                ].divide(const SizedBox(height: 8.0)),
+                  ].divide(const SizedBox(height: 8.0)),
+                ),
               ),
             ),
           ),
