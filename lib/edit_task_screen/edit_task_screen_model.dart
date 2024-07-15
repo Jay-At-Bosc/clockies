870093bf5c4420a3452a5740a82ad6a668876ff1
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-15 15:05:41 +0000

    Updating to latest FlutterFlow output.

--- lib/edit_task_screen/edit_task_screen_model.dart
+++ lib/edit_task_screen/edit_task_screen_model.dart
@@ -13,15 +13,7 @@ class EditTaskScreenModel extends FlutterFlowModel<EditTaskScreenWidget> {
 
   final unfocusNode = FocusNode();
   final formKey = GlobalKey<FormState>();
-  // State field(s) for TextField widget.
-  FocusNode? textFieldFocusNode1;
-  TextEditingController? textController1;
-  String? Function(BuildContext, String?)? textController1Validator;
   DateTime? datePicked1;
-  // State field(s) for TextField widget.
-  FocusNode? textFieldFocusNode2;
-  TextEditingController? textController2;
-  String? Function(BuildContext, String?)? textController2Validator;
   DateTime? datePicked2;
   DateTime? datePicked3;
 
@@ -31,10 +23,5 @@ class EditTaskScreenModel extends FlutterFlowModel<EditTaskScreenWidget> {
   @override
   void dispose() {
     unfocusNode.dispose();
-    textFieldFocusNode1?.dispose();
-    textController1?.dispose();
-
-    textFieldFocusNode2?.dispose();
-    textController2?.dispose();
   }
 }
