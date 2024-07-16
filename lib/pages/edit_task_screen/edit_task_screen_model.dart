1f9dcc5be467c3036f7787a10943ed0b8272556b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-16 06:22:30 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/pages/edit_task_screen/edit_task_screen_model.dart
@@ -0,0 +1,67 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'edit_task_screen_widget.dart' show EditTaskScreenWidget;
+import 'package:flutter/material.dart';
+
+class EditTaskScreenModel extends FlutterFlowModel<EditTaskScreenWidget> {
+  ///  Local state fields for this page.
+
+  DateTime? startDate;
+
+  DateTime? endDate;
+
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode1;
+  TextEditingController? textController1;
+  String? Function(BuildContext, String?)? textController1Validator;
+  DateTime? datePicked1;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode2;
+  TextEditingController? textController2;
+  String? Function(BuildContext, String?)? textController2Validator;
+  DateTime? datePicked2;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode3;
+  TextEditingController? textController3;
+  String? Function(BuildContext, String?)? textController3Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode4;
+  TextEditingController? textController4;
+  String? Function(BuildContext, String?)? textController4Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode5;
+  TextEditingController? textController5;
+  String? Function(BuildContext, String?)? textController5Validator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode6;
+  TextEditingController? textController6;
+  String? Function(BuildContext, String?)? textController6Validator;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    textFieldFocusNode1?.dispose();
+    textController1?.dispose();
+
+    textFieldFocusNode2?.dispose();
+    textController2?.dispose();
+
+    textFieldFocusNode3?.dispose();
+    textController3?.dispose();
+
+    textFieldFocusNode4?.dispose();
+    textController4?.dispose();
+
+    textFieldFocusNode5?.dispose();
+    textController5?.dispose();
+
+    textFieldFocusNode6?.dispose();
+    textController6?.dispose();
+  }
+}
