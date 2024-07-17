da1a851b5ba833d8e44832720e9b98557811876b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-17 10:21:49 +0000

    new theme add

--- lib/new_clockies/home/home_widget.dart
+++ lib/new_clockies/home/home_widget.dart
@@ -52,53 +52,81 @@ class _HomeWidgetState extends State<HomeWidget> {
             ),
             child: Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Align(
-                        alignment: const AlignmentDirectional(-1.0, -1.0),
-                        child: Text(
-                          dateTimeFormat('MMMMEEEEd', getCurrentTimestamp),
-                          style: FlutterFlowTheme.of(context)
-                              .headlineSmall
-                              .override(
-                                fontFamily: 'Inter',
-                                letterSpacing: 0.0,
-                              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Align(
+                          alignment: const AlignmentDirectional(-1.0, -1.0),
+                          child: Text(
+                            dateTimeFormat('MMMMEEEEd', getCurrentTimestamp),
+                            style: FlutterFlowTheme.of(context)
+                                .headlineSmall
+                                .override(
+                                  fontFamily: 'Inter',
+                                  letterSpacing: 0.0,
+                                ),
+                          ),
+                        ),
+                        Align(
+                          alignment: const AlignmentDirectional(-1.0, 0.0),
+                          child: Text(
+                            '${functions.selectGreetings(getCurrentTimestamp)}${FFAppState().user.userName}',
+                            style: FlutterFlowTheme.of(context)
+                                .headlineLarge
+                                .override(
+                                  fontFamily: 'Inter',
+                                  letterSpacing: 0.0,
+                                ),
+                          ),
+                        ),
+                      ].divide(const SizedBox(height: 16.0)),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      elevation: 1.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24.0),
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        height: 299.0,
+                        decoration: BoxDecoration(
+                          color:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                          borderRadius: BorderRadius.circular(24.0),
+                          border: Border.all(
+                            color: FlutterFlowTheme.of(context).secondaryText,
+                            width: 1.0,
+                          ),
                         ),
                       ),
-                      Align(
-                        alignment: const AlignmentDirectional(-1.0, 0.0),
-                        child: Text(
-                          '${functions.selectGreetings(getCurrentTimestamp)}${FFAppState().user.userName}',
-                          style: FlutterFlowTheme.of(context)
-                              .headlineLarge
-                              .override(
-                                fontFamily: 'Inter',
-                                letterSpacing: 0.0,
-                              ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      elevation: 1.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24.0),
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        height: 299.0,
+                        decoration: BoxDecoration(
+                          color:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                          borderRadius: BorderRadius.circular(24.0),
+                          border: Border.all(
+                            color: FlutterFlowTheme.of(context).secondaryText,
+                            width: 1.0,
+                          ),
                         ),
                       ),
-                    ].divide(const SizedBox(height: 16.0)),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 299.0,
-                    decoration: BoxDecoration(
-                      color: FlutterFlowTheme.of(context).secondaryBackground,
                     ),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 299.0,
-                    decoration: BoxDecoration(
-                      color: FlutterFlowTheme.of(context).secondaryBackground,
-                    ),
-                  ),
-                ].divide(const SizedBox(height: 20.0)),
+                  ].divide(const SizedBox(height: 20.0)),
+                ),
               ),
             ),
           ),
